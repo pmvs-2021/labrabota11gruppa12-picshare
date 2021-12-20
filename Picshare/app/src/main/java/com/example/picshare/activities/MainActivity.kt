b1181cb02522b68013ef3995c88323152f3b619a
zmitser <Zmitser.Horbach@itechart-group.com>
2021-12-20 23:43:11 +0300

    icons added for main activity

--- Picshare/app/src/main/java/com/example/picshare/activities/MainActivity.kt
+++ Picshare/app/src/main/java/com/example/picshare/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import com.example.picshare.R
 
