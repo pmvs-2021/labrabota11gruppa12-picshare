529d1ffc4b329f7e093026e409de38b768816db5
zmitser <Zmitser.Horbach@itechart-group.com>
2021-12-20 23:51:00 +0300

    the top bar with the app name is hidden

--- Picshare/app/src/main/java/com/example/picshare/activities/MainActivity.kt
+++ Picshare/app/src/main/java/com/example/picshare/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.picshare.activities
 
+import android.app.ActionBar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
         bindLateinits()
         setListeners()
     }
