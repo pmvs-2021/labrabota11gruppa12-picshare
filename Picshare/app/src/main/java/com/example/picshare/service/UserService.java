213b6e19e73d72dac7d7b46812f71f3e77f3560d
UzheNeKate <eavrub@gmail.com>
2021-12-22 21:59:16 +0300

    login activity created

--- Picshare/app/src/main/java/com/example/picshare/service/UserService.java
+++ Picshare/app/src/main/java/com/example/picshare/service/UserService.java
@@ -7,6 +7,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.RequestFuture;
 import com.android.volley.toolbox.StringRequest;
@@ -17,6 +18,8 @@ import com.google.gson.Gson;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -32,6 +35,18 @@ public class UserService {
         String url = String.format("https://%s/user?username=%s", Metadata.INSTANCE.getServerURL(), username);
         RequestFuture<User> future = RequestFuture.newFuture();
         UserRequest request = new UserRequest(Request.Method.GET, url, future, future);
+        Metadata.requests.add(request);
+        return future;
+    }
+
+    public static Future<JSONObject> addUser(String username) {
+        String url = String.format("https://%s/user", Metadata.INSTANCE.getServerURL());
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        RequestFuture<JSONObject> future = RequestFuture.newFuture();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url,
+                new JSONObject(params), future, future);
+
         Metadata.requests.add(request);
         return future;
     }
