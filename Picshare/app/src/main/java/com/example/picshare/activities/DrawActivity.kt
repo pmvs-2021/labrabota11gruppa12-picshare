008095ba443f2594f1bd77f26f96e676d07dc010
UzheNeKate <eavrub@gmail.com>
2021-12-23 14:10:09 +0300

    drawing added, chats activity added

--- Picshare/app/src/main/java/com/example/picshare/activities/DrawActivity.kt
+++ Picshare/app/src/main/java/com/example/picshare/activities/DrawActivity.kt
@@ -1,17 +1,25 @@
 package com.example.picshare.activities
 
-import android.app.ActionBar
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
-import com.android.volley.toolbox.Volley
-import com.example.picshare.Metadata.thisUser
-import com.example.picshare.Metadata.requests
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.drawToBitmap
+import com.byox.drawview.views.DrawView
 import com.example.picshare.R
-import com.example.picshare.domain.User
+import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
+import com.github.dhaval2404.colorpicker.model.ColorSwatch
+import java.io.File
+import java.io.FileOutputStream
+
 
 class DrawActivity : AppCompatActivity() {
     private lateinit var ibtChats: ImageButton
@@ -19,6 +27,9 @@ class DrawActivity : AppCompatActivity() {
     private lateinit var ibtSave: ImageButton
     private lateinit var ibtShare: ImageButton
     private lateinit var ibtSearch: ImageButton
+    private lateinit var ibtColor: ImageButton
+    private lateinit var dv: DrawView
+    private lateinit var sb: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +45,93 @@ class DrawActivity : AppCompatActivity() {
         ibtSave = findViewById(R.id.ibtSave)
         ibtShare = findViewById(R.id.ibtShare)
         ibtSearch = findViewById(R.id.ibtSearch)
+        ibtColor = findViewById(R.id.btnColor)
+        dv = findViewById(R.id.drawView)
+        sb = findViewById(R.id.seekBar)
     }
 
     private fun setListeners() {
         ibtSearch.setOnClickListener { v -> onSearchClick(v) }
+        ibtSave.setOnClickListener { v -> onSaveClick(v) }
+        ibtColor.setOnClickListener { v -> onColorClick(v) }
+        ibtChats.setOnClickListener { v -> onChatClick(v) }
+        sb.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                dv.drawWidth = progress * 2
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
     }
 
     private fun onSearchClick(v: View?) {
         val intent = Intent(this, SearchActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun onChatClick(v: View?) {
+        val intent = Intent(this, ChatsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onColorClick(v: View?) {
+        MaterialColorPickerDialog
+            .Builder(this)
+            .setColorShape(ColorShape.SQAURE)
+            .setColorSwatch(ColorSwatch._300)
+            .setColorListener { color, _ ->
+                dv.drawColor = color
+            }
+            .show()
+    }
+
+    private fun onSaveClick(v: View?) {
+        val file = File("picture")
+
+        try {
+            file.createNewFile()
+
+            val ostream = FileOutputStream(file)
+            val bitmap = getBitmapFromView(dv)
+            bitmap!!.compress(Bitmap.CompressFormat.PNG, 100, ostream)
+            ostream.flush()
+            ostream.close()
+        } catch (e: Exception) {
+            Toast.makeText(
+                applicationContext,
+                "Error saving file: " + e.message,
+                Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap? {
+        val bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+//        val state = dv.drawableState
+//        savedInstanceState.putByteArray("pic", state.toDrawable())
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    @Override
+    private fun onLoadInstanceState(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            val state = java.lang.Boolean.parseBoolean(savedInstanceState
+                .getString("pic", ""))
+            dv.drawToBitmap() //.drawableState = state
+        }
     }
 }
